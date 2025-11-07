184c41dd5dc928abe77ae486fcae969abab5cddd
dzakwanilhakim <79700185+dzakwanilhakim@users.noreply.github.com>
2025-11-07 13:50:24 +0700

    Update and rename tb.sh to tbpro.sh

--- tbpro.sh
+++ tbpro.sh
@@ -1,3 +1,4 @@
+# A. Installation
 # Create a new environment (optional but recommended)
 conda create -n tbpore_env python=3.10 -y
 conda activate tbpore_env
@@ -9,9 +10,12 @@ conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 conda install tbpore=0.7.1 -y
-tbpore download
+tbpore download # Will take time more than 1 hours
 
-# run
+# B. Run
+# run, download sample data from https://www.ebi.ac.uk/ena/browser/view/ERR9030361
+mkdir /mnt/d/TBPore
+cd /mnt/d/TBPore
 tbpore process ERR9030361.fastq.gz
 tbpore cluster ERR9030361.fastq.gz
 
