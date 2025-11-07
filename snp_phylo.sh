91e1c72ba652b7033424c452a0ed5b2f9a7dc273
dzakwanilhakim <79700185+dzakwanilhakim@users.noreply.github.com>
2025-11-07 15:08:53 +0700

    Create snp_phylo.sh

---
+++ snp_phylo.sh
@@ -0,0 +1,46 @@
+#tb_genomes/
+#├── H37Rv.fasta          # reference genome (download from NCBI)
+#├── TB1.fasta
+#├── TB2.fasta
+#├── TB3.fasta
+#├── ...
+#└── TB10.fasta
+
+# Install Environment
+conda create -n tb_phylo -c bioconda -c conda-forge parsnp trimal iqtree ete3 harvesttools -y
+conda activate tb_phylo
+
+mkdir -p /mnt/d/TBPhylo/tb_genomes
+cd /mnt/d/TBPhylo
+
+# 1. Run Parsnp
+# -r → reference genome (H37Rv is recommended for TB)
+# -d → directory with your FASTAs
+# -o → output directory
+ #-p → number of CPU threads
+parsnp -r tb_genomes/H37Rv.fasta -d tb_genomes -o tb_parsnp_out -p 2
+
+# 2. Convert .aln to fasta
+# The FASTA alignment is usually already parsnp.aln
+if [ -f tb_parsnp_out/parsnp.aln ]; then
+    cp tb_parsnp_out/parsnp.aln core_alignment.fasta
+else
+    harvesttools -x tb_parsnp_out/parsnp.xmfa -M core_alignment.fasta
+fi
+
+# 3. Trimming
+trimal -in core_alignment.fasta -out core_alignment_trimmed.fasta -automated1
+
+# 4. Phylogenetic tree inference with IQ-TREE
+# -s → input alignment
+# -m GTR+G → substitution model (good for SNPs)or use MFP (ModelFinder Plus)
+# -bb 1000 → ultrafast bootstrap with 1000 replicates
+# -nt AUTO → auto-detect cores
+# -pre tb_phylo → output prefix
+iqtree2 -s core_alignment_trimmed.fasta -m GTR+G -bb 1000 -nt AUTO -pre tb_phylo_trimmed
+
+# 5. Visualize
+ete3 view tb_phylo_trimmed.treefile
+
+
+
