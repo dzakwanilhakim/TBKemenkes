d73ac6ef6cba6d4482bd3d361cd182e54e01eca0
dzakwanilhakim <79700185+dzakwanilhakim@users.noreply.github.com>
2025-11-10 10:35:13 +0700

    Update mykrobe-env.sh

--- TBPore/mykrobe-env.sh
+++ TBPore/mykrobe-env.sh
@@ -1,8 +1,16 @@
-conda create -n mykrobe-env
-conda install mykrobe
+conda create -n mykrobe-env -y
+conda install mykrobe gdown -y
 
 mykrobe predict sample \
   --species tb \
   --input sample.fasta \
   --format fasta \
   --output sample_mykrobe.json
+
+# 4. download json2xlsx converter
+gdown "https://drive.google.com/uc?id=1OcbhUwv85LWR4c3aI_hNXK7bcS6gD6uf"
+chmod +x json2xlsx
+sed -i 's/\r$//' json2xlsx
+
+# 5. convert mykrobe.json to mykrobe.xlsx
+./json2xlsx sample_mykrobe.json
