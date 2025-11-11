7499508602d1066574fafd94c28e9abf663dfc8c
dzakwanilhakim <79700185+dzakwanilhakim@users.noreply.github.com>
2025-11-11 16:43:10 +0700

    Add tbphylo_partition.sh for phylogenetic analysis
    This script automates the process of running parsnp for alignment, converting the output, trimming the alignment, constructing a phylogenetic tree, and visualizing the results.

---
+++ TBPhylo/tbphylo_partition.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#├── Reference/ H37Rv.fasta          # reference genome (download from NCBI)
+#├── TB1.fasta
+#├── TB2.fasta
+#├── TB3.fasta
+#├── ...
+#└── TB10.fasta
+
+# Alignment
+read -p "Enter number of threads to use (min: 1): " P
+read -p "MSA method (muscle/mafft): " N
+read -p "Partition size (min: 50): " MIN
+echo "1) Running parsnp with $part threads, msa with $msa, partition size $part"
+
+parsnp -r tb_genomes/Reference/H37Rv.fasta \
+       -d tb_genomes \
+       -o tb_parsnp_out \
+       -p $P \
+       -n $N \
+       --min-partition-size $MIN
+       
+# Convert
+echo "2) Convert"
+if [ -f tb_parsnp_out/parsnp.aln ]; then
+    cp tb_parsnp_out/parsnp.aln core_alignment.fasta
+else
+    harvesttools -x tb_parsnp_out/parsnp.xmfa -M core_alignment.fasta
+fi
+
+# Trimming
+echo "3) Trimming"
+trimal -in core_alignment.fasta -out core_alignment_trimmed.fasta -automated1
+
+# Tree Consctruction
+echo "4) Tree Construction"
+iqtree -s core_alignment_trimmed.fasta -m GTR+G -bb 1000 -nt AUTO -pre tb_phylo_trimmed
+
+# Visualization
+echo "5) Visualization"
+ete3 view -t tb_phylo_trimmed.treefile --image tree.png
+ete3 view -t tb_phylo_trimmed.treefile
